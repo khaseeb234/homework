8752aada860766430cf1967542877a65c0561cfc
khaseeb234 <bahirabdul00@gmail.com>
2026-05-29 00:49:14 +0300

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,43 @@
+'''
+#1
+user_number=int(input("Ввидите число"))
+user_number_2=int(input("Ввидите число"))
+user_number_3=int(input("Ввидите число"))
+summa=user_number+user_number_3+user_number_3
+product=user_number*user_number_2*user_number_3
+print(f"Сумма: ",summa)
+print(f"Произвдение: ",product)
+'''
+'''
+#2
+salary=float(input("Зарплата за месяц"))
+credit=float(input("Ежемесячный платеж по кредиту в банке"))
+communal=float(input("Задолжность за коммунальные услуги"))
+remainder=salary-credit-communal
+print(f"Остаток: ",remainder)
+'''
+#3
+d1=float(input("Длина первой диагонали"))
+d2=float(input("Длина второй диагонали"))
+result=d1*d2/2
+print(f"Результат вычисление: ",result)
+'''
+#4
+text=str("\t\t To Be")
+text_2=str("\t\t or not")
+text_3=str("\t\t to be")
+print(text)
+print(text_2)
+print(text_3)
+'''
+'''
+#5
+text_4=str("\t«Life is what happens")
+text_5=str("\t    when")
+text_6=str("\t\tyou're busy making other plans»")
+text_7=str("\t\t\t\t\tJohn Lennon")
+print(text_4)
+print(text_5)
+print(text_6)
+print(text_7)
+'''
